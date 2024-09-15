9f4cf014b4819d976027901a2750cc41414f9dd7
Mansi Maurya <51503378+mansi75@users.noreply.github.com>
2024-09-15 17:48:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_store_openai.pkl"
 
 main_placeholder = st.empty()
-llm = OpenAI(temperature=0.9, max_tokens=500, model_name=“gpt-3.5-turbo-instruct”)
+llm = OpenAI(temperature=0.9, max_tokens=500, model_name="gpt-3.5-turbo-instruct")
 
 if process_url_clicked:
     # load data
