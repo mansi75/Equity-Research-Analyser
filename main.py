1cf4068cd771d56fbddad9074d03fa022153e3d5
Mansi Maurya <51503378+mansi75@users.noreply.github.com>
2024-09-30 01:20:29 +0530

    Identation added

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ load_dotenv()
 
 st.title("EquityBot: News Research Tool 📈")
 st.sidebar.title("news Article URLs")
-
 urls = []
 for i in range(3):
-url = st.sidebar.text_input(f"URL {i+1}")
-urls.append(url)
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
 
 process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_store_openai.pkl"
@@ -28,62 +27,66 @@ main_placeholder = st.empty()
 llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.9, max_tokens=500)
 
 if process_url_clicked:
-# load data
-loader = UnstructuredURLLoader(urls=urls)
-main_placeholder.text("Data Loading...Started...✅✅✅")
-data = loader.load()
-# split data
-text_splitter = RecursiveCharacterTextSplitter(
-separators=['\n\n', '\n', '.', ','],
-chunk_size=1000
-)
-main_placeholder.text("Text Splitter...Started...✅✅✅")
-docs = text_splitter.split_documents(data)
-# create embeddings and save it to FAISS index
-embeddings = OpenAIEmbeddings()
-vectorstore_openai = FAISS.from_documents(docs, embeddings)
-main_placeholder.text("Embedding Vector Started Building...✅✅✅")
-time.sleep(2)
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = OpenAIEmbeddings()
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
 
-def generate_response(prompt):
-response = openai.ChatCompletion.create(
-model="gpt-3.5-turbo",  # Use the correct model name
-messages=[{"role": "user", "content": prompt}],
-temperature=0.9,
-max_tokens=500
+    def generate_response(prompt):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # Use the correct model name
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.9,
+            max_tokens=500
 
-)
-return response.choices[0].message['content']
-
-
-class CustomChain(RetrievalQAWithSourcesChain):
-def _call(self, inputs, return_only_outputs=False):
-question = inputs["question"]
-# Generate the response using the chat model
-response = generate_response(question)
-return {"answer": response}
+        )
+        return response.choices[0].message['content']
 
 
-# Save the FAISS index to a pickle file
-with open(file_path, "wb") as f:
-pickle.dump(vectorstore_openai, f)
+    class CustomChain(RetrievalQAWithSourcesChain):
+        def _call(self, inputs, return_only_outputs=False):
+            question = inputs["question"]
+        # Generate the response using the chat model
+            response = generate_response(question)
+            return {"answer": response}
+
+
+    # Save the FAISS index to a pickle file
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
 
 query = main_placeholder.text_input("Question: ")
 if query:
-if os.path.exists(file_path):
-with open(file_path, "rb") as f:
-vectorstore = pickle.load(f)
-#chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-chain = CustomChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-result = chain({"question": query}, return_only_outputs=True)
-# result will be a dictionary of this format --> {"answer": "", "sources": [] }
-st.header("Answer")
-st.write(result["answer"])
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            #chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            chain = CustomChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
 
-# Display sources, if available
-sources = result.get("sources", "")
-if sources:
-st.subheader("Sources:")
-sources_list = sources.split("\n")  # Split the sources by newline
-for source in sources_list:
-st.write(source)
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
+
+
+
+
